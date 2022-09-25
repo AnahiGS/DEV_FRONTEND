b5bc134f317ae8f4957e91260b0bba4109e4361d
Anahi Garcia Soria <anahi.gsoria@gmail.com>
2022-09-25 15:41:08 -0500

    se agregó un proyecto con vite

---
+++ intro-react/class-components-vite/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+class App extends Component {
+  render (){
+    return (
+      <div className="App">
+        <h3>React con class components </h3>
+      </div>
+    );
+  }
+}
+  
+
+  
+
+
+export default App
