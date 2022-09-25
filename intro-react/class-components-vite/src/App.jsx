ad05922d12014cc7a6ec413daac82691f2f294b4
Anahi Garcia Soria <anahi.gsoria@gmail.com>
2022-09-25 16:51:38 -0500

    se agregaron botones

--- intro-react/class-components-vite/src/App.jsx
+++ intro-react/class-components-vite/src/App.jsx
@@ -1,13 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { Component, Fragment } from 'react'
 import './App.css'
 
+//1 entender las características de component
 class App extends Component {
+  state={
+    counter:0
+  }
+  handleClick = (type) =>{
+    if(type ==="inc") this.setState({ counter: this.state.counter +1});
+    else{
+      if(this.state.counter >0){
+        this.setState({
+          counter: this.state.counter -1.
+        });
+      }
+    }
+  }
+  // handleClickIncrement = () =>{
+  //   this.setState({counter: this.state.counter +1})
+  // }
+  // handleClickDecrement = () =>{
+  //   this.setState({counter: this.state.counter -1})
+  // }
+  //2 poner el método render
   render (){
+
+    //JSX
     return (
-      <div className="App">
+      <>
         <h3>React con class components </h3>
-      </div>
+        <div className="App">
+          <span> Contador {this.state.counter}</span>
+          <div className="btn-section">
+            <button onClick={() => this.handleClick("inc")}>Incrementar</button>
+            <button onClick={() => this.handleClick ("dec")}>Decrementar</button>
+
+          </div>
+        </div>
+      </>
+      // <div className="App">
+      //   <h3>React con class components </h3>
+      // </div>
     );
   }
 }
