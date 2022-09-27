3aa7dd6e8343d54fb4bdb468a8821a0650f4572b
Anahi Garcia Soria <anahi.gsoria@gmail.com>
2022-09-27 09:05:31 -0500

    added css

--- intro-react/pequeño-proyecto-vite/src/App.jsx
+++ intro-react/pequeño-proyecto-vite/src/App.jsx
@@ -1,9 +1,16 @@
 import React, { Component } from 'react'
+import FormComponent from './components/FormComponent'
+import NavbarComponent from './components/NavbarComponent'
+import BadgeComponent from './components/BadgeComponent'
 
 export class App extends Component {
   render() {
     return (
-      <div>App</div>
+      <div className="app">
+        <NavbarComponent/>
+        <FormComponent/>
+        <BadgeComponent/>
+      </div>
     )
   }
 }
--- intro-react/pequeño-proyecto-vite/src/components/NavbarComponent.jsx
+++ intro-react/pequeño-proyecto-vite/src/components/NavbarComponent.jsx
@@ -3,7 +3,14 @@ import React, { Component } from 'react'
 export class NavbarComponent extends Component {
   render() {
     return (
-      <div>NavbarComponent</div>
+      <div className="Navbar">
+        <div className= "container-fluid">
+          <div className="Navbar_brand">
+            <span className="font-weight-light">Devf</span>
+            <span className="font-weight-bold">Online</span>
+          </div>
+        </div>
+      </div>
     )
   }
 }
