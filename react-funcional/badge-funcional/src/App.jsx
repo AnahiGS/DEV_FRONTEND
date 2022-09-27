b86d608e98cc13b5aa2fcba96ebde06935a44cf7
Anahi Garcia Soria <anahi.gsoria@gmail.com>
2022-09-27 18:13:01 -0500

    se agregaron los componentes de axios

--- react-funcional/badge-funcional/src/App.jsx
+++ react-funcional/badge-funcional/src/App.jsx
@@ -1,18 +1,42 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import BadgeComponent from './components/BadgeComponent'
 import FormComponent from './components/FormComponent'
 import Navbar from './components/Navbar'
+import AxiosComponent from './components/AxiosComponent'
+import axios from 'axios'
 
-
-function App() {
-  const [form, setForm] =useState({
+const App = () => {
+  const [form, setForm] = useState({
     firstName: '',
     lastName: '',
-    username: '',
     jobTitle: '',
+    username: '',
+    rol: ''
   })
 
-  const handleChange =(event)=>{
+  const [characters, setCharacters] = useState([])
+
+
+
+  const fetchApi = () => {
+    axios.get("https://rickandmortyapi.com/api/character").then(res => {
+      const characters = res.data.results
+      setCharacters(characters)
+    })
+  }
+   // useEffect (CICLO DE VIDA HOOK)
+   useEffect(() => {
+    // Se ejecuta cuando se monta y se actualiza (props/state)
+    fetchApi()
+  });
+  
+  useEffect(() => {
+    return () => {
+      // Se ejecuta cuando se desmonta
+    }
+  });
+
+  const handleChange = (event) => {
     setForm({
       ...form,
       [event.target.name]: event.target.value
@@ -20,10 +44,17 @@ function App() {
   }
 
   return (
-    <div className="App">
+  
+    <div>
       <Navbar/>
       <FormComponent handleChange={handleChange}/>
       <BadgeComponent form={form}/>
+      {
+        characters.map((item,index) => {
+          return <AxiosComponent item={item} key={`${index}-axios`} />
+        })
+      }
+      
     </div>
   )
 }
